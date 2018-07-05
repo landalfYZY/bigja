ad04b731160fd3f26196843ac08031a56b22d191
landalf <landalf@sunwou.com>
2018-07-05 17:09:27 +0800

    yui
    567

--- src/components/gallery/gallery.vue
+++ src/components/gallery/gallery.vue
@@ -5,28 +5,44 @@
                 <ButtonGroup>
                     <Button type="ghost">删除</Button>
                     <Button type="ghost" @click="centerDialogVisible = true">导入图片</Button>
-                    <Button type="ghost">分类标签</Button>
+                    <Button type="ghost" >分类标签</Button>
                 </ButtonGroup>
             </div>
             <Button type="ghost">刷新</Button>
         </div>
+
         <el-dialog title="上传图片" :visible.sync="centerDialogVisible" width="400px" center>
-            <Upload multiple :action="api + 'file/fileup'">
+            <Upload multiple :action="api + 'file/fileup'" :max-size="2048" :before-upload="beforeUpload">
                 <Button type="ghost" icon="ios-cloud-upload-outline">点击上传图片</Button>
             </Upload>
-            <span slot="footer" class="dialog-footer">
+            <span slot="footer" class="dialog-footer" >
                 <Button type="primary" @click="centerDialogVisible = true" long>确定</Button>
             </span>
         </el-dialog>
+
+        <div class="ult-wiss">
+            
+        </div>
+
     </div>
 </template>
 <script>
+var that ;
 export default {
-    data(){
-        return {
-            api:this.com.API,
-            centerDialogVisible:false
-        }
-    }
-}
+  data() {
+    return {
+      api: this.com.API,
+      centerDialogVisible: false
+    };
+  },
+  mounted() {
+      that = this;
+
+  },
+  methods:{
+      beforeUpload(e){
+          console.log(e)
+      },
+  }
+};
 </script>
